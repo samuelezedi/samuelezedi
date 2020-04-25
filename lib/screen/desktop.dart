31862c3c7ee53a407dbc1c10232a911879eeb308
Samuel Ezedi <samuelezedi@gmail.com>
2020-04-25 19:02:26 +0100

    basic

---
+++ lib/screen/desktop.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:linkwell/linkwell.dart';
+
+class DeskTopView extends StatelessWidget {
+  var constraints;
+  DeskTopView(this.constraints);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(30),
+      child: Column(children: <Widget>[
+        Expanded(child: Container(
+          width: constraints.biggest.width / 2,
+                  child: ListView(
+                    children: <Widget>[
+                      Text(
+                        "I'm a Software Developer, with immense specialty in Flutter for Mobile and Web, PHP Laravel for Backend AS A Service. Also I'm Music Art Junkie and futurist"
+                      ),
+                      LinkWell(
+                        "I'm current the CTO at https://pronoun.com.ng, a startup online educational platform for students in Nigeria.",
+                        listOfNames: {
+                          'https://pronoun.com.ng' : 'ProNoun',
+                        },
+                        linkStyle: TextStyle(color: Colors.green),
+                      ),
+                      LinkWell(
+                        "I'm an aspiring tech leader, speaker and problem solver, I have written quiet a number of technical articles on medium and still writing."
+                      ),
+                      LinkWell(
+                        "https://github.com/samuelezedi https://medium.com/@samuelezedi https://twitter.com/samuelezedi https://instagram.com/samuelezedi",
+                        listOfNames: {
+                          "https://github.com/samuelezedi" : "Github",
+                          "https://medium.com/@samuelezedi" : "Medium",
+                          "https://twitter.com/samuelezedi" : "Twitter",
+                          "https://instagram.com/samuelezedi" : "Instagram"
+                        },
+                        linkStyle: TextStyle(color: Colors.green),
+                      )
+                    ],
+                  ),
+        ),),
+        RichText(text: TextSpan(
+          children: [
+            TextSpan(text: "Samuel"),
+            TextSpan(text: "Ezedi",style: TextStyle(fontWeight: FontWeight.bold))
+          ]
+          ),)
+      ],),
+    );
+  }
+}
--- lib/screen/index.dart
+++ lib/screen/index.dart
@@ -9,8 +9,22 @@ class IndexPage extends StatefulWidget {
 class _IndexPageState extends State<IndexPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return LayoutBuilder(
+      builder: (context, constraints){
+        if (constraints.maxWidth > 1200) {
+          //computers
+          return DesktopView(constraints);
+        } else if (constraints.maxWidth > 805 && constraints.maxWidth < 1200) {
+          //tablets
+          return TabletView();
+        } else if (constraints.maxWidth > 400 && constraints.maxWidth < 805) {
+          //tablets
+          return SmallTabletView();
+        } else {
+          //mobile views
+          return MobileView();
+        }
+      },
+      )
   }
 }

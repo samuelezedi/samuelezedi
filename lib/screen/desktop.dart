bbd3f0426525390bdda0275fbde04936a6a48eae
Samuel Ezedi <samuelezedi@gmail.com>
2020-04-25 20:51:44 +0100

    few more files in assets

--- lib/screen/desktop.dart
+++ lib/screen/desktop.dart
@@ -7,46 +7,55 @@ class DeskTopView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(30),
-      child: Column(children: <Widget>[
-        Expanded(child: Container(
-          width: constraints.biggest.width / 2,
-                  child: ListView(
-                    children: <Widget>[
-                      Text(
-                        "I'm a passionate Software Developer, with immense specialty in Flutter for Mobile and Web, PHP Laravel for Backend AS A Service. Also a Music Art Junkie and futurist",
-                        style: TextStyle(color: Colors.black),
-                      ),
-                      LinkWell(
-                        "I'm currently the CTO at https://pronoun.com.ng, a online startup educational platform for students in Nigeria.",
-                        listOfNames: {
-                          'https://pronoun.com.ng' : 'ProNoun',
-                        },
-                        linkStyle: TextStyle(color: Colors.green),
-                      ),
-                      LinkWell(
-                        "I'm an aspiring tech leader, speaker and problem solver, I have written quiet a number of technical articles on medium and still writing."
-                      ),
-                      LinkWell(
-                        "https://github.com/samuelezedi https://medium.com/@samuelezedi https://twitter.com/samuelezedi https://instagram.com/samuelezedi",
-                        listOfNames: {
-                          "https://github.com/samuelezedi" : "Github",
-                          "https://medium.com/@samuelezedi" : "Medium",
-                          "https://twitter.com/samuelezedi" : "Twitter",
-                          "https://instagram.com/samuelezedi" : "Instagram"
-                        },
-                        linkStyle: TextStyle(color: Colors.green, decoration: TextDecoration.underline),
-                      )
-                    ],
-                  ),
-        ),),
-        RichText(text: TextSpan(
-          children: [
-            TextSpan(text: "Samuel",style: TextStyle(fontSize: 200, fontWeight: FontWeight.bold)),
-            TextSpan(text: "Ezedi",style: TextStyle(fontSize: 200, fontWeight: FontWeight.bold))
-          ]
-          ),)
-      ],),
+      color: Colors.lightGreen,
+      child: Container(
+        margin: EdgeInsets.all(30),
+        child: Column(children: <Widget>[
+          Expanded(child: Container(
+            width: constraints.biggest.width / 2,
+                    child: ListView(
+                      children: <Widget>[
+                        Text(
+                          "I'm a passionate Software Developer, with immense specialty in Flutter for Mobile and Web, PHP Laravel for Backend AS A Service. Also a Music Art Junkie and futurist",
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        LinkWell(
+                          "I'm currently the CTO at https://pronoun.com.ng, a online startup educational platform for students in Nigeria.",
+                          listOfNames: {
+                            'https://pronoun.com.ng' : 'ProNoun',
+                          },
+                          linkStyle: TextStyle(color: Colors.green),
+                        ),
+                        LinkWell(
+                          "I'm an aspiring tech leader, speaker and problem solver, I have written quiet a number of technical articles on medium and still writing."
+                        ),
+                        LinkWell(
+                          "https://github.com/samuelezedi https://medium.com/@samuelezedi https://twitter.com/samuelezedi https://instagram.com/samuelezedi",
+                          listOfNames: {
+                            "https://github.com/samuelezedi" : "Github",
+                            "https://medium.com/@samuelezedi" : "Medium",
+                            "https://twitter.com/samuelezedi" : "Twitter",
+                            "https://instagram.com/samuelezedi" : "Instagram"
+                          },
+                          linkStyle: TextStyle(color: Colors.green, decoration: TextDecoration.underline),
+                        )
+                      ],
+                    ),
+          ),),
+          Container(
+            width: double.infinity,
+            child: FittedBox(
+              fit: BoxFit.contain,
+                child: RichText(text: TextSpan(              
+                children: [
+                  TextSpan(text: "Samuel ",),
+                  TextSpan(text: "Ezedi",style: TextStyle(fontWeight: FontWeight.bold))
+                ]
+                ),),
+            ),
+          )
+        ],),
+      ),
     );
   }
 }
